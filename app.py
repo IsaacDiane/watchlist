9decc1b92fde70d0180d7e3a98e8caf15c0e6d1a
Jack <1239062183@qq.com>
2020-09-27 13:43:17 +0800

    add home page

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
+from flask import url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '欢迎'
+
+@app.route('/whoami')
+def whoami():
+    return 'whoami'
+
+@app.route('/users/<name>')
+def user(name):
+    return 'hello'+name
+
+@app.route('/test_url_for')
+def test():
+    print(url_for('hello_world',num=2))
+    print(url_for('user',name=u'张三'))
+    return 'look at terminal!'
 
 
 if __name__ == '__main__':
